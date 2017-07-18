720c05512f9b0128dde690f62bab8a8374a1bb89
AlexanderSolovyev <1430@list.ru>
2017-07-18 11:36:31 +0300

    validate for answers

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,21 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  let(:question){create(:question)}
+  let(:user){ create(:user) }
   describe 'POST #create' do
-    let(:question){create(:question)}
+    
     context 'with valid attributes' do
+      example 'unregistered user cant saves answer' do
+        post :create, params: { answer: attributes_for(:answer), question_id: question, format: :js }
+        expect(response.body).to include 'You need to sign in or sign up before continuing.'
+      end
 
       it 'saves the new answer in db' do
+        sign_in user
         expect{ post :create, params: { answer: attributes_for(:answer), question_id: question, format: :js }}.to change(question.answers, :count).by(1) 
       end
 
       it 'render view' do
+        sign_in user
         post :create, params: { answer: attributes_for(:answer), question_id: question, format: :js }
         expect(response).to render_template :create 
       end
     end
 
     context 'with invalid attributes' do
+      before{sign_in user}
 
       it 'does no save answer' do
         expect{ post :create, params: { answer: attributes_for(:invalid_answer), question_id: question, format: :js }}.to_not change(question.answers, :count) 
--- spec/features/crud_answer_spec.rb
+++ spec/features/crud_answer_spec.rb
@@ -11,4 +11,29 @@ feature 'user can crud answers for questions' do
 
     expect(page).to have_content 'MyAnswer'
   end
+
+  scenario 'un-authenticate user cant create answer', js: true do
+    visit question_path(question)
+
+    expect(page).not_to have_content 'You answer'
+
+  end
+
+  scenario 'user cant create invalid answer', js: true do
+    sign_in user
+    visit question_path(question)
+    click_on 'Send answer'
+
+    expect(page).to have_content "Body can't be blank"
+  end
+
+  scenario 'user can edit his answer' do
+    sign_in user
+    visit question_path(question)
+    click_on 'Edit answer'
+    fill_in 'answer[body]', with: 'My new answer'
+    click_on 'Done'
+    
+    expect(page).to have_content 'My new answer'
+  end
 end
